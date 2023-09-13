04bf7fcae3f81ac49b3036a5b0a64422d1914881
James (DPC) <603960+MrJamesEllis@users.noreply.github.com>
2023-09-13 16:36:56 +1000

    Split out class into separate file

---
+++ src/Forms/ColourField.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Codem\Utilities\HTML5;
+
+/**
+ * Provides a class for those of us not using en_US
+ */
+class ColourField extends ColorField {
+
+    protected $template = "Codem/Utilities/HTML5/ColorField";
+
+}
