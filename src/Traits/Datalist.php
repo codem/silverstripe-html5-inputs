1d6b8bf6c74adb24a4461993b7985bc1c7ff6373
James <603960+MrJamesEllis@users.noreply.github.com>
2025-06-27 12:45:00 +1000

    Declare default property value

--- src/Traits/Datalist.php
+++ src/Traits/Datalist.php
@@ -12,7 +12,7 @@ use SilverStripe\View\ArrayData;
 trait Datalist
 {
 
-    protected ?ArrayList $inputDatalist;
+    protected ?ArrayList $inputDatalist = null;
 
     protected string $inputDatalistId = '';
 
